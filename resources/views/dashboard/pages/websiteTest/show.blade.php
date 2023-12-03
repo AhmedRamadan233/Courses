14df1cf59700fc2e704508f3e36c01dc2bc6d20e
Ahmed Ramadan <ahmedromio233@gmail.com>
2023-12-03 13:02:16 +0200

    edit in websitetest

--- resources/views/dashboard/pages/websiteTest/show.blade.php
+++ resources/views/dashboard/pages/websiteTest/show.blade.php
@@ -9,86 +9,106 @@
 
 @section('content')
 
-
-<div class="row">
-    <div class="card-body">
-        <!-- Video Container -->
-        <div id="video-container" class="embed-responsive embed-responsive-16by9" style="width: 800px; height: 438px;">
-            <iframe id="video" class="embed-responsive-item" src="{{ asset('upload/' . $showCategory->video) }}" type="video/mp4" frameborder="0" allowfullscreen></iframe>
-        </div>
-
-        <!-- Categories -->
-        <div class="categories mt-3">
-            <h4>Categories:</h4>
-            <div class="btn-group" role="group" aria-label="Categories">
-                <button type="button" class="btn btn-secondary category" data-target="description-card">Description</button>
-                <button type="button" class="btn btn-secondary category" data-target="common-question-card">Common Question</button>
-                <button type="button" class="btn btn-secondary category" data-target="courses-container-card">Courses Container</button>
-                <button type="button" class="btn btn-secondary category" data-target="rate-card">Rate</button>
-                <button type="button" class="btn btn-secondary category" data-target="others-card">Others</button>
-            </div>
-        </div>
-
-        <div class="col-lg-12">
-            <!-- Description Card -->
-            <div id="description-card" class="card card-primary card-outline" style="display: none;">
+<div class="d-flex justify-content-between align-items-center">
+    <div class="row">
+        <div class="col-md-8 p-5 ">
+            <div class="card" style="width: 800px;">
                 <div class="card-header text-center">
-                    <h5>Description</h5>
+                    <h5>video</h5>
                 </div>
-                <div class="card-body">
-                    @foreach ($categoryDescriptions as $category)
-                        @if ($category->description)
-                            @foreach ($category->description as $description)
-                                <h5>{{ $description->question }}</h5>
-                                <p>{{ $description->answer }}</p>
-                            @endforeach
-                        @endif
-                    @endforeach
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-12">
-            <!--sections -->
-            <div id="courses-container-card" class="card card-primary card-outline" style="display: none;">
-                <div class="card-header text-center">
-                    <h5>Courses Sections</h5>
-                </div>
-                <div class="card-body">
-                    @foreach ($categoryDescriptions as $category)
-                        @if ($category->sections)
-                            <div class="btn-group" role="group" aria-label="Categories">
-                                @foreach ($category->sections as $cousesSection)
-                                    <div class="category border border-light p-3 mb-2 bg-dark rounded text-center text-light">
-                                        <a href=""><h2>{{ $cousesSection->name }}</h2></a>
-                                    </div>
-                                @endforeach
+                <div class="card-body col-md-12 p-3">
+                    <!-- Video Container -->
+                    <div id="video-container" class="embed-responsive embed-responsive-16by9" style="width: 760px; height: 450px;">
+                        <iframe id="video" class="embed-responsive-item" src="{{ asset('upload/' . $showCategory->video) }}" type="video/mp4" frameborder="0" allowfullscreen></iframe>
+                    </div>
+        
+                    <!-- Categories -->
+                    <div class="categories mt-3">
+                        <h4>Categories:</h4>
+                        <div class="btn-group" role="group" aria-label="Categories">
+                            <button type="button" class="btn btn-secondary category" data-target="description-card">Description</button>
+                            <button type="button" class="btn btn-secondary category" data-target="common-question-card">Common Question</button>
+                            <!-- Uncomment the following lines if needed -->
+                            {{-- <button type="button" class="btn btn-secondary category" data-target="courses-container-card">Courses Container</button> --}}
+                            <button type="button" class="btn btn-secondary category" data-target="rate-card">Rate</button>
+                            <button type="button" class="btn btn-secondary category" data-target="others-card">Others</button>
+                        </div>
+                    </div>
+        
+                    <div class="row">
+                        <div class="col-lg-12 pt-3">
+                            <!-- Description Card -->
+                            <div id="description-card" class="card card-primary card-outline" style="display: none;">
+                                <div class="card-header text-center">
+                                    <h5>Description</h5>
+                                </div>
+                                <div class="card-body">
+                                    @foreach ($categoryDescriptions as $category)
+                                        @if ($category->description)
+                                            @foreach ($category->description as $description)
+                                                <h5>{{ $description->question }}</h5>
+                                                <p>{{ $description->answer }}</p>
+                                            @endforeach
+                                        @endif
+                                    @endforeach
+                                </div>
                             </div>
-                        @endif
-                    @endforeach
+                        </div>
+                        <div class="col-lg-12">
+                            <!-- Common Question Card -->
+                            <div id="common-question-card" class="card card-primary card-outline" style="display: none;">
+                                <div class="card-header text-center">
+                                    <h5>Common Question</h5>
+                                </div>
+                                <div class="card-body">
+                                    @foreach ($categoryDescriptions as $category)
+                                        @if ($category->commonQestions)
+                                            @foreach ($category->commonQestions as $commonQestions)
+                                                <h5>{{ $commonQestions->question }}</h5>
+                                                <p>{{ $commonQestions->answer }}</p>
+                                            @endforeach
+                                        @endif
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-lg-12">
-            <!-- Common Question Card -->
-            <div id="common-question-card" class="card card-primary card-outline" style="display: none;">
+        
+        {{--  --}}
+        <div class="col-md-4 p-5">
+            <!-- Sections Card -->
+            <div class="card">
                 <div class="card-header text-center">
-                    <h5>Common Question</h5>
+                    <h5>Sections</h5>
                 </div>
                 <div class="card-body">
-                    @foreach ($categoryDescriptions as $category)
-                        @if ($category->commonQestions)
-                            @foreach ($category->commonQestions as $commonQestions)
-                                <h5>{{ $commonQestions->question }}</h5>
-                                <p>{{ $commonQestions->answer }}</p>
-                            @endforeach
-                        @endif
-                    @endforeach
+                    <div>
+                        @foreach ($categoryDescriptions as $category)
+                            @if ($category->sections)
+                                <div class="accordion" id="sectionsAccordion">
+                                    @foreach ($category->sections as $index => $courseSection)
+                                        <div class="category border border-light p-3 mb-2 bg-dark rounded text-center text-light" data-bs-toggle="collapse" data-bs-target="#sectionCollapse{{ $index }}">
+                                            <a href="#"><h2>{{ $courseSection->name }}</h2></a>
+                                        </div>
+                                        <div id="sectionCollapse{{ $index }}" class="collapse">
+                                            {{-- git  --}}
+                                        </div>
+                                    @endforeach
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <!-- Footer content goes here -->
                 </div>
             </div>
         </div>
     </div>
 </div>
-
 
 @endsection
 @push('webste.scripts')
