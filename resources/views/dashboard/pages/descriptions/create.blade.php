94a0d0357e8a4b2f3b37d77a26b23b0ed1d55967
Ahmed Ramadan <ahmedromio233@gmail.com>
2023-12-06 11:15:46 +0200

    fixed bug in descriptions route

--- resources/views/dashboard/pages/descriptions/create.blade.php
+++ resources/views/dashboard/pages/descriptions/create.blade.php
@@ -81,7 +81,7 @@
             var parentId = $(this).val();
 
             $.ajax({
-                url: '/dashboard/description/get_parents/' + parentId,
+                url: '/dashboard/descriptions/get_parents/' + parentId,
                 type: 'GET',
                 success: function(data) {
                     $('#category_id').empty();
